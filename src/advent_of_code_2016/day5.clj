3a3fbac87473ee7b4b7c118f8ce4dafd1e56f074
Alexander Riese <alexander.riese@student.hpi.de>
2017-09-20 21:31:56 +0200

    add part two of day5 puzzle

--- src/advent_of_code_2016/day5.clj
+++ src/advent_of_code_2016/day5.clj
@@ -16,9 +16,18 @@
         raw (.digest algorithm (.getBytes s))]
     (format "%032x" (BigInteger. 1 raw))))
 
+(defn fill-password [password data]
+  (let [[character position] (first data)]
+    (cond
+      (not-any? nil? password) password
+      (nil? (nth password position)) (recur (assoc-in password [position] character) (rest data))
+      :else (recur password (rest data)))))
+
 (->> (positive-numbers)
      (map #(str door-id %))
      (map md5)
      (filter #(= (subs % 0 5) "00000"))
-     (map #(nth % 5))
-     (take 8))
+     (filter #(<= 0 (Character/digit (nth % 5) 10) 7))
+     (map (juxt #(nth % 6) #(Character/digit (nth % 5) 10)))
+     (fill-password [nil nil nil nil nil nil nil nil]))
+
